c636ce070128fb7ebb278692060e8e7f593c0ce8
flickerinhouse <rtemelkovski@yahoo.com>
2015-11-02 22:47:55 -0500

    Added basic skeleton for all the Readable related files

---
+++ Book.java
@@ -0,0 +1,18 @@
+public class Book extends ​Readable {
+	
+	public Book(int price,int sNo,String authorName){
+		super(price,sNo,authorName);
+	}
+
+	@Override
+	// this won't work cause 1.02 is a float
+	public int getPrice(){
+		return 1.02 * price;
+	}
+
+	public int getListInfo(){
+		//Based on the value of Type(Book or eBook) print the list of Items
+		// What does that mean ?
+	}
+
+}
--- Item.java
+++ Item.java
@@ -1,5 +1,17 @@
 package eCommerce;
 
+// ask about 2% tax always being an int for readables
+
+// in Book.java ask about below statement
+// Based on the value of Type(Book or eBook) print the list of Items
+// What does that mean ?
+
+// Book OO design says just add 2 % but under billing info, it has HST and shipping
+
+// 	In eBook.java	
+// override and only call the parent’s constructor to get the base price.
+// ask about what that means
+
 public abstract class Item{
 	// fields
 	protected int price;
