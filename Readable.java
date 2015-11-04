32dce37d4ccd7fb40b0dc0aa262a335e4776c618
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-11-04 11:58:09 -0500

    Readable.java

--- Readable.java
+++ Readable.java
@@ -1,3 +1,9 @@
+/*
+ * Name: Nick Morrison, Roberto Temelkovski, Teo Voinea
+ * MacID: morrin2, temelkr, voineat
+ * Student Number: 1426613, 1418731, 1409586
+ * Description: File containing the Readable Class
+ */
 import java.util.*;
 
 public class Readable extends Item {
