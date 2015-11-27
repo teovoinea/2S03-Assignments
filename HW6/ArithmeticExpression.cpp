0ceeebb7d1d4bf4c2edc919c613b93d075096aca
teovoinea <voinea.teodor@gmail.com>
2015-11-27 15:00:31 -0500

    Added all the .cpp and .h files with the header comment

---
+++ HW6/ArithmeticExpression.cpp
@@ -0,0 +1,6 @@
+/*
+* Name: Nick Morrison, Roberto Temelkovski, Teo Voinea
+* MacID: morrin2, temelkr, voineat
+* Student Number: 1426613, 1418731, 1409586
+* Description: File containing the Arithmetic Expression class
+*/
